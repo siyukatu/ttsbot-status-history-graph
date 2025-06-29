15b58d14fe689d3235e8fada8b2aac3bf9ee7072
siyukatu <siyukatu@gmail.com>
2025-06-29 22:28:07 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
- 
+import os
+import glob
+
+for p in glob.glob('output/*.png'):
+    if os.path.isfile(p):
+        os.remove(p)
+
 x = np.arange(-5, 5, 0.1)
 y = np.sin(x)
- 
+
 plt.plot(x, y)
- 
-plt.savefig("output/test.png")
+
+plt.savefig("output/test2.png")
